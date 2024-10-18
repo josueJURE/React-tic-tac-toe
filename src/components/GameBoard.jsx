3442c1da28aba5f3e18f9b4abe9be22f00464017
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2024-10-18 11:13:25 +0100

    (feat: JS) add logic to GameBaord()

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 
 const initialGameBoard = [
     [null, null, null],
@@ -6,11 +8,23 @@ const initialGameBoard = [
 ]
 
 function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleClickButton(rowIndex, colIndex) {
+        setGameBoard((prevGameBaord) => {
+            const updatedBoard = [...prevGameBaord.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = "X";
+            return updatedBoard
+        })
+        console.log("btn clicked");
+    }
+
+
     return (
     <ol id="game-board">
-     {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+     {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
         <ol>
-            {row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li> )}
+            {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={() => handleClickButton(rowIndex, colIndex)}>{playerSymbol}</button></li> )}
         </ol>
      </li> )}
 
